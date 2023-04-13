df0bb7e8dc06addc6c8ba0a9ac77e37252fb9e41
Shaji Khan <quotablecode@gmail.com>
2023-04-13 23:18:46 +0530

    almost complete

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from ui import UI
+from models.object import ObjectDetect
 import gi
+import sys
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 ui = UI ()
+object_detect = ObjectDetect ()
+path = ui.download_and_resize_image (sys.argv [1])
+img = object_detect.run_detector (path)
+ui.set_image (img)
+
 Gtk.main ()
--- ui.py
+++ ui.py
@@ -1,8 +1,22 @@
 import gi
+import array
 import os
+import tempfile
+from six.moves.urllib.request import urlopen
+from six import BytesIO
+import numpy as np
+from PIL import Image
+from PIL import ImageColor
+from PIL import ImageDraw
+from PIL import ImageFont
+from PIL import ImageOps
+import io
+
+# For measuring the inference time.
+import time
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, GdkPixbuf
+from gi.repository import Gtk, GdkPixbuf, Gio, GLib
 
 class UI ():
     window = None
@@ -21,3 +35,29 @@ class UI ():
         
     def __init__ (self):
         self.create_window ()
+
+    def download_and_resize_image(self, url, new_width=256, new_height=256):
+        _, filename = tempfile.mkstemp(suffix=".jpg")
+        response = urlopen(url)
+        image_data = response.read()
+        image_data = BytesIO(image_data)
+        pil_image = Image.open(image_data)
+        pil_image = ImageOps.fit(pil_image, (new_width, new_height), Image.ANTIALIAS)
+        pil_image_rgb = pil_image.convert("RGB")
+        pil_image_rgb.save(filename, format="JPEG", quality=90)
+        print("Image downloaded to %s." % filename)
+        return filename
+
+    def pillow_to_pixbuf (self, image):
+        contents = None
+        with io.BytesIO() as output:
+            image.save(output, format="JPEG")
+            contents = output.getvalue()
+        _b = GLib.Bytes.new (contents)
+        stream = Gio.MemoryInputStream.new_from_bytes(_b)
+        pixbuf = GdkPixbuf.Pixbuf.new_from_stream(stream, None)
+        return pixbuf
+
+    def set_image (self, img):
+        pixbuf = self.pillow_to_pixbuf (img)
+        self.img.set_from_pixbuf (pixbuf)
