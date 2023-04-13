93c2d3ac55eec5776a104238e2bb6f73c6f740ff
Shaji Khan <git@shaji.in>
2023-04-13 19:41:24 +0530

    default logo

--- ui.py
+++ ui.py
@@ -1,15 +1,23 @@
 import gi
+import os
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, GdkPixbuf
 
 class UI ():
     window = None
     def create_window (self):
         self.window = Gtk.Window ()
-        self.window.set
+        self.window.resize (800, 600)
+        self.box = Gtk.VBox ()
+        self.window.add (self.box)
+        self.img = Gtk.Image ()
+        self.img.set_from_pixbuf (GdkPixbuf.Pixbuf.new_from_file (os.getcwd () + "/logo.png"))
+        self.box.pack_start (self.img, True, True, 10)
+        self.entry = Gtk.Entry ()
+        self.box.pack_start (self.entry, False, False, 10)
         self.window.show_all ()
         self.window.connect("destroy", Gtk.main_quit)
         
     def __init__ (self):
-
+        self.create_window ()
